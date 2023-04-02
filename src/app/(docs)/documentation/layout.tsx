5e292d45c242a5e86a0070dc67d481dbf2ced505
Marjan02 <kevinzevalja@gmail.com>
2023-04-02 12:51:38 +0700

    first commit

---
+++ src/app/(docs)/documentation/layout.tsx
@@ -0,0 +1,6 @@
+import { ReactNode } from "react";
+
+export default function Layout({ children }: { children: ReactNode }) {
+  return <section className="pt-20">{children}</section>;
+}
+
---
+++ src/app/(docs)/documentation/page.tsx
@@ -0,0 +1,27 @@
+import { FC } from "react";
+
+import type { Metadata } from "next";
+import Heading from "@/components/ui/Heading";
+import Paragraph from "@/components/ui/Paragraph";
+import DocumentationTabs from "@/components/DocumentationTabs";
+import "simplebar-react/dist/simplebar.min.css";
+
+export const metadata: Metadata = {
+  title: "Similarity API | Documentation",
+  description: "Free & open-source text similarity API",
+};
+
+const page: FC = ({}) => {
+  return (
+    <div className="container max-w-7xl mx-auto mt-12">
+      <div className="flex flex-col items-center gap-6">
+        <Heading>Making a request</Heading>
+        <Paragraph>api/v1/similarity</Paragraph>
+
+        <DocumentationTabs />
+      </div>
+    </div>
+  );
+};
+
+export default page;
