17e16e273d1a15f7392fc155867a1f134825a4d7
Marjan02 <kevinzevalja@gmail.com>
2023-04-03 08:45:25 +0700

    benerin api

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,32 +1,30 @@
+import { getToken } from "next-auth/jwt";
+import { withAuth } from "next-auth/middleware";
+import { NextResponse } from "next/server";
 import { Ratelimit } from "@upstash/ratelimit";
 import { Redis } from "@upstash/redis";
-import { getToken } from "next-auth/jwt";
-import withAuth from "next-auth/middleware";
-import { NextResponse } from "next/server";
 
 const redis = new Redis({
   url: process.env.REDIS_URL,
   token: process.env.REDIS_SECRET,
 });
 
-const retelimit = new Ratelimit({
-  redis,
-  limiter: Ratelimit.slidingWindow(20, "1 h"),
+const ratelimit = new Ratelimit({
+  redis: redis,
+  limiter: Ratelimit.slidingWindow(50, "1 h"),
 });
 
 export default withAuth(
   async function middleware(req) {
-    const pathname = req.nextUrl.pathname; //relative path
+    const pathname = req.nextUrl.pathname; // relative path
 
-    // Manage rete limiting
+    // Manage rate limiting
     if (pathname.startsWith("/api")) {
       const ip = req.ip ?? "127.0.0.1";
       try {
-        const { success } = await retelimit.limit(ip);
+        const { success } = await ratelimit.limit(ip);
 
-        if (!success) {
-          return NextResponse.json({ error: "Too many requests" });
-        }
+        if (!success) return NextResponse.json({ error: "Too Many Requests" });
         return NextResponse.next();
       } catch (error) {
         return NextResponse.json({ error: "Internal Server Error" });
@@ -36,8 +34,7 @@ export default withAuth(
     // Manage route protection
     const token = await getToken({ req });
     const isAuth = !!token;
-
-    const isAuthPage = pathname.startsWith("/login");
+    const isAuthPage = req.nextUrl.pathname.startsWith("/login");
 
     const sensitiveRoutes = ["/dashboard"];
 
@@ -59,6 +56,9 @@ export default withAuth(
   {
     callbacks: {
       async authorized() {
+        // This is a work-around for handling redirect on auth pages.
+        // We return true here so that the middleware function above
+        // is always called.
         return true;
       },
     },
@@ -66,5 +66,5 @@ export default withAuth(
 );
 
 export const config = {
-  mather: ["/", "/login", "/dashboard/:path*", "/api/:path*"],
+  matcher: ["/", "/login", "/dashboard/:path*", "/api/:path*"],
 };
