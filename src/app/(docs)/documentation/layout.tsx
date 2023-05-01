a44935b62f78696b0ef649d53156ad146c24e888
Marjan02 <kevinzevalja@gmail.com>
2023-05-01 08:52:21 +0700

    clean code

--- src/app/(docs)/documentation/layout.tsx
+++ src/app/(docs)/documentation/layout.tsx
@@ -3,4 +3,3 @@ import { ReactNode } from "react";
 export default function Layout({ children }: { children: ReactNode }) {
   return <section className="pt-20">{children}</section>;
 }
-
--- src/app/dashboard/error.tsx
+++ src/app/dashboard/error.tsx
@@ -1,20 +1,21 @@
-'use client'
+"use client";
 
-import { Button } from '@/components/ui/Button'
-import Paragraph from '@/components/ui/Paragraph'
+import { Button } from "@/components/ui/Button";
+import Paragraph from "@/components/ui/Paragraph";
 
 export default function Error({ reset }: { error: Error; reset: () => void }) {
   return (
-    <div className='flex flex-col gap-4 items-center'>
+    <div className="flex flex-col gap-4 items-center">
       <Paragraph>Something went wrong while loading this page.</Paragraph>
       <Button
-        size='lg'
+        size="lg"
         onClick={
           // Attempt to recover by trying to re-render the segment
           () => reset()
-        }>
+        }
+      >
         Try again
       </Button>
     </div>
-  )
+  );
 }
